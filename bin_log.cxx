4e27952e78e83bf6d036b8d2c7d3b798615287c2
hamidreza maddah <hamidrezamaddah@yahoo.com>
2022-11-07 10:14:11 +0330

    add descriptor

--- bin_log.cxx
+++ bin_log.cxx
@@ -2,6 +2,133 @@
 #include <iostream>
 #include <vector>
 #include <iterator>
+#include <cstring>
+#include <sstream>
+#include <typeinfo>
+#include <variant>
+#include <any>
+#include <map>
+
+// oxygen_level 89 timestamp-U32 level-D other_data-I32
+// system_state 88 arg1-I8 state2-I32 timestamp-U32
+// sensor_data 80 data1-D data2-f data3-I32 timestamp-U32
+
+std::vector<std::string> string_split(const std::string &str, char delemiter)
+{
+    std::vector<std::string> strings;
+    unsigned int start = 0;
+    while (true)
+    {
+        auto idx = str.find_first_of(delemiter, start);
+        idx = std::min(idx, str.length());
+        auto len = idx - start;
+        std::string sub = str.substr(start, len);
+        strings.push_back(sub);
+        if (idx == str.length())
+        {
+            break;
+        }
+        start = idx + 1;
+    }
+    return strings;
+}
+
+enum param_types_t
+{
+    none,
+    U8,
+    I8,
+    U16,
+    I16,
+    U32,
+    I32,
+    D,
+    F
+};
+
+std::map<std::string, param_types_t> param_types{
+    {"U8", param_types_t::U8},
+    {"I8", param_types_t::I8},
+    {"U16", param_types_t::U16},
+    {"I16", param_types_t::I16},
+    {"U32", param_types_t::U32},
+    {"I32", param_types_t::I32},
+    {"F", param_types_t::F},
+    {"D", param_types_t::D}};
+
+struct param_t
+{
+    std::string name;
+    param_types_t type;
+};
+
+struct pkt_descriptor_t
+{
+    std::string name;
+    unsigned int id;
+    std::vector<param_t> params;
+};
+
+std::vector<pkt_descriptor_t> parse_descriptor()
+{
+    std::vector<pkt_descriptor_t> descriptors;
+    std::ifstream file("pktinfo.txt");
+    std::string line;
+    while (std::getline(file, line))
+    {
+        pkt_descriptor_t descriptor;
+        std::istringstream iss(line);
+        iss >> descriptor.name;
+        iss >> descriptor.id;
+
+        while (true)
+        {
+            std::string param_str;
+            iss >> param_str;
+            std::vector<std::string> split_res = string_split(param_str, '-');
+            if (split_res.size() != 2)
+            {
+                break;
+            }
+            param_t param{split_res[0], param_types[split_res[1]]};
+            descriptor.params.push_back(param);
+        }
+        descriptors.push_back(descriptor);
+    }
+}
+
+struct pkt_t
+{
+    uint8_t id;
+    uint8_t cs;
+    uint16_t seq;
+    std::vector<uint8_t> content;
+};
+
+// void print_pkts(const std::vector<pkt_t> &pkts, std::vector<pkt_descriptor_t> descriptors)
+// {
+//     unsigned int num = 1;
+//     bool first = true;
+//     unsigned int init_sec;
+//     unsigned int num_lost = 0;
+//     for (auto &pkt : pkts)
+//     {
+//         std::cout << "PKT" << std::dec << num++ << " ID=" << std::hex << (int)pkt.id << " Seq=0x" << pkt.seq << " Content=" << pkt.content << "\n";
+//         if (first)
+//         {
+//             first = false;
+//         }
+//         else
+//         {
+//             if (pkt.seq != init_sec + 1)
+//             {
+//                 num_lost += (pkt.seq - init_sec - 1);
+//             }
+//         }
+//         init_sec = pkt.seq;
+//     }
+//     std::cout << "PktLoss=" << std::dec << num_lost;
+// }
 
 long long int get_file_size(std::ifstream &fin)
 {
@@ -17,14 +144,6 @@ enum class state_t
     h1,
     h2,
     id,
-};
-
-struct pkt_t
-{
-    uint8_t id;
-    uint8_t cs;
-    uint16_t seq;
-    std::vector<uint8_t> content;
 };
 
 std::ostream &operator<<(std::ostream &os, const std::vector<uint8_t> &items)
@@ -73,7 +192,7 @@ uint8_t check_sum(uint8_t *buff, unsigned int len)
 
 int main(int argc, char **argv)
 {
-    std::ifstream fin("sample.bin", std::ios::binary); 
+    std::ifstream fin("pkts.bin", std::ios::binary);
     fin.unsetf(std::ios::skipws);
     unsigned int file_size = get_file_size(fin);
     std::cout << "file size: " << file_size << "\n";
@@ -134,7 +253,6 @@ int main(int argc, char **argv)
             state = state_t::h1;
 
             break;
-
         }
     }
     print_pkts(pkts);
